312673efe7ce2c598b122edf0e542128cd60e36c
Krue1 <879924250@qq.com>
2020-08-19 14:35:23 +0800

    新推送

---
+++ Vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+    <p>{{message}} world!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "wow",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
