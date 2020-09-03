fbea14908a0e44f69b77acb81d410006d498c8b1
Krue1 <879924250@qq.com>
2020-09-03 14:32:42 +0800

    新更改

--- Vue/myfirstvue/src/views/Login.vue
+++ Vue/myfirstvue/src/views/Login.vue
@@ -6,15 +6,24 @@
       <el-radio-button label="top">顶部对齐</el-radio-button>
     </el-radio-group>
     <div style="margin: 20px;"></div>
-    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
-      <el-form-item label="名称">
+    <el-form
+      ref="formLabelAlign"
+      :label-position="labelPosition"
+      label-width="80px"
+      :model="formLabelAlign"
+    >
+      <el-form-item label="名称" prop="name">
         <el-input v-model="formLabelAlign.name"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域">
+      <el-form-item label="活动区域" prop="region">
         <el-input v-model="formLabelAlign.region"></el-input>
       </el-form-item>
-      <el-form-item label="活动形式">
+      <el-form-item label="活动形式" prop="type">
         <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+      <el-form-item size="large">
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -32,6 +41,15 @@ export default {
         type: ""
       }
     };
+  },
+  methods: {
+    onSubmit() {
+      console.log("yes");
+    },
+    reset() {
+      this.$refs["formLabelAlign"].resetFields();
+      this.formLabelAlign.type = "男";
+    }
   }
 };
 </script>
--- Vue/myfirstvue/src/views/Music.vue
+++ Vue/myfirstvue/src/views/Music.vue
@@ -1,9 +1,48 @@
 <template>
-  <h3>音乐</h3>
+  <div id="music" ref="music">
+    <h3>音乐</h3>
+    <el-button @click="clickHandler">进行操作</el-button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      moves: "URLRDL",
+      moves2: "RRRRRL",
+      keys: new Set()
+    };
+  },
+  methods: {
+    clickHandler() {
+      console.log(this.moves.split("L"));
+      console.log(this.moves2.split("R"));
+      this.runOnKeys(() => alert("Hello!"), "KeyQ", "KeyW");
+    },
+    runOnKeys(func, ...codes) {
+      let _this = this;
+      this.$refs["music"].addEventListener("keydown", function(e) {
+        console.log(e.code);
+        let flag = true;
+        _this.keys.add(e.code);
+        codes.forEach(code => {
+          if (!_this.keys.has(code)) {
+            flag = false;
+            return;
+          }
+        });
+        if (flag) {
+          func();
+          _this.keys.clear();
+        }
+      });
+      this.$refs["music"].addEventListener("keyup", function(e) {
+        _this.keys.delete(e.code);
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
